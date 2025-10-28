8ad3edcc0ac4164e6cf4a53821ede644c56a85af
AbdulazizErgashev <abdulazizergashev0335@gmail.com>
2025-10-28 20:08:09 +0500

    Home created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,106 @@
-import React from 'react'
-
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <section className="bg-black">
+      <div className="container mx-auto grid grid-cols-3 gap-20 py-10">
+        <iframe
+          width="450"
+          height="315"
+          src="https://www.youtube.com/embed/7rQfGeI7VeE?si=FBiTuMdPw4MQXf9E"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+
+        <iframe
+          width="450"
+          height="315"
+          src="https://www.youtube.com/embed/nb_fFj_0rq8?si=av1ON9QU_hpkERep"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+
+        <iframe
+          width="450"
+          height="315"
+          src="https://www.youtube.com/embed/7ESZWdXlkRw?si=C3AY2yHML96vDJlW"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+
+        <iframe
+          width="450"
+          height="315"
+          src="https://www.youtube.com/embed/2YzrFdLXBDc?si=ohv1JRelsyL4bN6b"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+
+        <iframe
+          width="450"
+          height="315"
+          src="https://www.youtube.com/embed/LhRXf-yEQqA?si=44vkdsdaezVmJCeE"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+
+        <iframe
+          width="450"
+          height="315"
+          src="https://www.youtube.com/embed/sg5WQ3N5qj4?si=8H4en3hTthzTbzY4"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+
+        <iframe
+          width="450"
+          height="315"
+          src="https://www.youtube.com/embed/jY98EwB1IJQ?si=SrT_fL7FSLCtEK9C"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+
+        <iframe
+          width="450"
+          height="315"
+          src="https://www.youtube.com/embed/W7RCqRabt8M?si=6beo8vDpk2BtgVKB"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+
+        <iframe
+          width="450"
+          height="315"
+          src="https://www.youtube.com/embed/BpP-8TLwb6I?si=kDspbjrkqfwnTQeZ"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </section>
+  );
 }
